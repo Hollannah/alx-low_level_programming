117a04ead272085b5404a3587909cf7ee26ca2f2
hollannah <olaideanna@gmail.com>
2022-12-12 17:55:08 -0800

    im feeling sleepy

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print out sized of dataa types in c
+ * code by hollannah
+ * Return: 0
+*/
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+
+	printf("size of a char: %lu byte(S)\n", (unsigned long)sizeof (a));
+	printf("size of an int: %lu bytes(S)\n", (unsigned long)sizeof (a));
+	printf("size of a long int: %lu byte(S)\n", (unsigned long)sizeof (a));
+	printf("size of a long long int: %lu byte(S)\n", (unsigned long)sizeof (a));
+	printf("size of a float: %lu byte(S)\n", (unsigned long)sizeof (a));
+	return (0);
+}
