fdb1f0782c5fa5f26fc4b55c18949ef8d832a36a
hollannah <olaideanna@gmail.com>
2022-12-19 16:11:11 -0800

    Task 5

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,33 +1,17 @@
-nclude "main.h"
+#include "main.h"
 
 /**
+ * puts2 - prints every other character of a string
+ * @str: char to check
  *
- *  * puts2 - prints every other character of a string
- *
- *   *
- *
- *    * @str: char to check
- *
- *     *
- *
- *      * Return: 0 is success
- *
- *       */
-
+ * Return: 0 is success
+ */
 void puts2(char *str)
-
 {
+	int string;
 
-		int string;
-
-
-
-			for (string = 0; str[string] != '\0'; string++)
-
-					if (string % 2 == 0)
-
-								_putchar(str[string]);
-
-				_putchar('\n');
-
+	for (string = 0; str[string] != '\0'; string++)
+		if (string % 2 == 0)
+			_putchar(str[string]);
+	_putchar('\n');
 }
