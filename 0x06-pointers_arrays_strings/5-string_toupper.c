d6caa76a2a38ff367e53b96ea797fbb33521ff32
hollannah <olaideanna@gmail.com>
2022-12-29 17:29:16 -0800

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,41 +1,21 @@
-nclude "main.h"
+#include "main.h"
 
 /**
- *
- *  * string_toupper - change lowercase to uppercase
- *
- *   * @s:string
- *
- *    * Return:char
- *
- *     */
-
+ * string_toupper - change lowercase to uppercase
+ * @s:string
+ * Return:char
+ */
 char *string_toupper(char *s)
-
 {
 
+	int i;
 
-
-		int i;
-
-
-
-		i = 0;
-
-			while (*(s + i))
-
-					{
-
-								if (*(s + i) >= 'a' && *(s + i) <= 'z')
-
-												*(s + i) -= 'a' - 'A';
-
-										i++;
-
-											}
-
-				return (s);
-
+i = 0;
+	while (*(s + i))
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+			*(s + i) -= 'a' - 'A';
+		i++;
+	}
+	return (s);
 }
-
-/*Bwave ICT */
