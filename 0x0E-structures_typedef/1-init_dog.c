149074d460df093ab2874992747f49046240f2a8
Hollannah <118212140+Hollannah@users.noreply.github.com>
2023-01-31 14:25:22 +0100

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,39 +1,23 @@
-nclude "dog.h"
-
-
+#include "dog.h"
 
 /**
+ * init_dog - initialize dog struct
  *
- *  * init_dog - initializes a structure of type dog
+ * @d: input pointer to struct dog
+ * @name: input for dog name
+ * @age: input for dog age
+ * @owner: input for dog owner
  *
- *   * @d: pointer to structure
- *
- *    * @name: pointer to the name of the dog
- *
- *     * @age: age of dog
- *
- *      * @owner: pointer to owner of dog
- *
- *       *
- *
- *        * Return: void
- *
- *         */
+ * Return: nothing
+*/
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
-
 {
-
-		if (d)
-
-				{
-
-							d->name = name;
-
-									d->age = age;
-
-											d->owner = owner;
-
-												}
-
+	if (d)
+	{
+		/*d->name = name;*/
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
+	}
 }
